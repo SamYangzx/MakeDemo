a931c2186bdef4f8b45cfd69830bd4ba681c170e
yangzhuoxun <zhuoxun.yang@gometech.com.cn>
2018-06-14 17:05:16 +0800

    调试alertDialog

--- Android.mk
+++ Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_PACKAGE_NAME := HelloAndroid
 
 LOCAL_USE_FRAMEWORK_GOME:= true
--- src/shy/luo/hello/HelloAndroid.java
+++ src/shy/luo/hello/HelloAndroid.java
@@ -5,8 +5,12 @@ import android.app.admin.DevicePolicyManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 import android.content.Intent;
 import android.view.View;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
 import com.android.settings.password.ChooseLockGeneric;
 import com.android.settings.password.ChooseLockSettingsHelper;
 
@@ -31,17 +35,18 @@ public class HelloAndroid extends Activity {
             @Override
             public void onClick(View v) {
                 Log.i(LOG_TAG, "testBtn is clicked.");
-                Intent intent = new Intent();
-//                intent.setClassName("com.android.settings", "com.android.settings.password.ChooseLockGeneric");
-                intent.setClassName("com.android.settings", "com.android.settings.PasswordManagerActivity");
-//                intent.putExtra(ChooseLockGeneric.ChooseLockGenericFragment.MINIMUM_QUALITY_KEY,
-//                        DevicePolicyManager.PASSWORD_QUALITY_SOMETHING);
-//                intent.putExtra(ChooseLockGeneric.ChooseLockGenericFragment.HIDE_DISABLED_PREFS, true);
-                intent.putExtra("has_challenge",true);
-                intent.putExtra("challenge", 0);
-                intent.putExtra("for_fingerprint", true);
-                intent.putExtra("for_lock_security", true);
-                startActivityForResult(intent, CHOOSE_LOCK_GENERIC_REQUEST);
+                testAlertDialog();
+//                Intent intent = new Intent();
+////                intent.setClassName("com.android.settings", "com.android.settings.password.ChooseLockGeneric");
+//                intent.setClassName("com.android.settings", "com.android.settings.PasswordManagerActivity");
+////                intent.putExtra(ChooseLockGeneric.ChooseLockGenericFragment.MINIMUM_QUALITY_KEY,
+////                        DevicePolicyManager.PASSWORD_QUALITY_SOMETHING);
+////                intent.putExtra(ChooseLockGeneric.ChooseLockGenericFragment.HIDE_DISABLED_PREFS, true);
+//                intent.putExtra("has_challenge", true);
+//                intent.putExtra("challenge", 0);
+//                intent.putExtra("for_fingerprint", true);
+//                intent.putExtra("for_lock_security", true);
+//                startActivityForResult(intent, CHOOSE_LOCK_GENERIC_REQUEST);
             }
         });
     }
@@ -55,4 +60,35 @@ public class HelloAndroid extends Activity {
             startActivity(intent);
         }
     }
+
+    private void testAlertDialog() {
+        final String items[] = {"男", "未知", "女"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);  //先得到构造器
+        builder.setTitle(R.string.test);                                     //设置标题
+//        builder.setIcon(R.mipmap.ic_launcher);                      //设置图标，图片id即可
+
+        //设置单选按钮
+        //  items   为列表项
+        //  0       为默认选中第一个
+        //  第三个参数是监听器
+        builder.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //dialog.dismiss();
+                Toast.makeText(HelloAndroid.this, items[which], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //  设置监听器
+        builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(HelloAndroid.this, R.string.yes, Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.create().show();
+
+    }
+
 }
